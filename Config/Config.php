b2770e8df85a60224ffe3297cbfdc2025ede35ef
steveAV1 <steve_m77@hotmail.fr>
2024-10-28 21:58:01 +0000

    añadir todos los ficheros

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://localhost/biblio/";
+const base_url = "http://10.2.5.215/biblioteca/";
 const host = "localhost";
-const user = "root";
-const pass = "";
+const user = "biblioteca";
+const pass = "usuario";
 const db = "biblioteca";
 const charset = "charset=utf8";
 ?>
